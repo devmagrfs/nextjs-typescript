91331092ed1324b1e1f0e2372abdfc53a2709959
devmagrfs <devmagrfs@gmail.com>
2022-10-04 03:00:00 +0900

    feat: remark 및 matter 라이브러리 사용해서 md를 html 형식으로 변환

--- lib/post.ts
+++ lib/post.ts
@@ -1,6 +1,8 @@
 import fs from 'fs';
 import path from 'path';
 import matter from 'gray-matter';
+import { remark } from 'remark';
+import remarkHtml from 'remark-html';
 
 const postsDirectory = path.join(process.cwd(), 'posts');
 console.log('process.cwd()', process.cwd());
@@ -49,4 +51,19 @@ export function getAllPostIds() {
 			},
 		};
 	});
+}
+
+export async function getPostData(id: string) {
+	const fullPath = path.join(postsDirectory, `${id}.md`);
+	const fileContents = fs.readFileSync(fullPath, 'utf8');
+
+	const matterResult = matter(fileContents);
+	const processedContent = await remark().use(remarkHtml).process(matterResult.content);
+	const contentHtml = processedContent.toString();
+
+	return {
+		id,
+		contentHtml,
+		...(matterResult.data as { date: string; title: string }),
+	};
 }
